e1192f53b04db3d673d8f4ae23e1f72123df0bf9
Matias Ceau <matias@ceau.net>
2025-02-03 10:18:01 +0100

    chore: Update .gitignore and refactor CSV method.
    - Add .notebooks/ to .gitignore file.
    - Refactor CSV reading method to enhance clarity and consistency in data processing.

--- src/psyris_code/preprocessing/csv_to_json.py
+++ src/psyris_code/preprocessing/csv_to_json.py
@@ -10,17 +10,17 @@ class CSVToJSON:
 
     def __init__(self, path: Path):
         self.path = path
-        self.csv = self.read_as_csv()
+        self.df = self._read_csv()
         self.json = self.convert_csv_to_json()
 
-    def read_as_csv(self):
+    def _read_csv(self):
         df = pd.read_csv(self.path, sep=",", encoding="utf-8")
         df.columns = [Utils.clean_variable_name(col) for col in df.columns]
         return df
 
     def convert_csv_to_json(self):
         documents = []
-        for _, row in self.csv.iterrows():
+        for _, row in self.df.iterrows():
             doc = {row.name: row.to_dict()}
             documents.append(doc)
         return documents
